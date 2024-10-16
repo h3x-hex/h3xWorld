d76f6e31baf5e5ab99689f989230bd58b2c8942a
h3x-hex <160248326+h3x-hex@users.noreply.github.com>
2024-10-16 09:23:08 +0100

    Update Home
    Update

--- client/src/Routes/Home.jsx
+++ client/src/Routes/Home.jsx
@@ -2,12 +2,23 @@ import { useState } from 'react'
 import StarsCanvas from '../components/Stars'
 import { useMediaQuery } from 'react-responsive'
 import {useNavigate} from 'react-router-dom'
+import { getDatabase, ref, set } from "firebase/database";
 
 
 function Home() {
   const [count, setCount] = useState(0)
   const isMobile = useMediaQuery({ query: '(max-width: 520px)' })
   const navigate = useNavigate();
+
+  const [email, setEmail] = useState('');
+  const [addedToWaitlist, setAddedToWaitlist] = useState(false);
+
+  function addUserToWaitlist(){
+    const timestamp = new Date().getTime();
+    const db = getDatabase();
+    set(ref(db, `Waitlist/${timestamp}`), email);
+    setAddedToWaitlist(true);
+  }
 
   return (
     <>
@@ -19,21 +30,19 @@ function Home() {
     <div className='flex flex-col pt-16 gap-16 pl-3 '>
       <div className='flex mx-auto'>
         <h1 className='text-5xl'>Welcome to h<span className='text-[#d69e2e]'>3</span>x<span className='text-[#888888]'>|</span>World</h1>
+        <h1 className='text-2xl'>Web3 Social Media</h1>
       </div>
       <div className='flex flex-col mx-auto pr-3 gap-3'>
         <div className='flex flex-col gap-3 items-center justify-center'>
-          <h1  className='text-xl mx-auto pb-3'>Join h<span className='text-[#d69e2e]'>3</span>x<span className='text-[#888888]'>|</span>World</h1>
-          <label className="input-group">
-            <span className='text-xl'>h3x.world/</span>
-            <input type="text" placeholder="username" className="input input-bordered text-lg text-black" />
-          </label>
-          <button className='btn btn-warning w-80'>Join h3x|World</button>
-        </div>
-        <div className='divider divider-warning'></div>
-        <div className='flex flex-col gap-3'>
-          <input type="text" placeholder="Username" className="input input-bordered text-lg w-80" />
-          <input type="password" placeholder="Password" className="input input-bordered text-lg w-80" />
-          <button className='btn btn-warning w-80' onClick={() => navigate('/register')}>Enter h3x|World</button>
+          {
+            addedToWaitlist ?
+
+            <h1  className='text-xl mx-auto pb-3'>Joined the h<span className='text-[#d69e2e]'>3</span>x<span className='text-[#888888]'>|</span>World beta waitlist. Thank you and see you soon!</h1>
+            :
+            <h1  className='text-xl mx-auto pb-3'>Join the h<span className='text-[#d69e2e]'>3</span>x<span className='text-[#888888]'>|</span>World beta waitlist.</h1>
+          }
+          <input type="text" placeholder="Email" className="input input-bordered text-lg text-black w-80" disabled={addedToWaitlist} value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <button className='btn btn-warning w-80' onClick={() => addUserToWaitlist()} disabled={addedToWaitlist}>Join h3x|World</button>
         </div>
       </div>
     </div>
@@ -49,23 +58,21 @@ function Home() {
     <div className='h-screen bg-stone-950 text-white'>
       <div className='h-screen relative z-0'>
       <div className='flex flex-col pt-36 gap-36 '>
-        <div className='flex mx-auto'>
+        <div className='flex flex-col mx-auto text-center'>
           <h1 className='text-[96px]'>Welcome to h<span className='text-[#d69e2e]'>3</span>x<span className='text-[#888888]'>|</span>World</h1>
+          <h1 className='text-2xl'>Web3 Social Media for creators, artists, and entrepreneurs.</h1>
         </div>
         <div className='flex flex-row mx-auto pr-3 gap-8'>
-          <div className='flex flex-col gap-3'>
-            <h1  className='text-3xl mx-auto pb-3'>Join h<span className='text-[#d69e2e]'>3</span>x<span className='text-[#888888]'>|</span>World</h1>
-            <label className="input-group">
-              <span className='text-2xl'>h3x.world/</span>
-              <input type="text" placeholder="username" className="input input-bordered text-2xl text-black" />
-            </label>
-            <button className='btn btn-warning'>Join h3x|World</button>
-          </div>
-          <div className='divider divider-warning divider-horizontal'></div>
-          <div className='flex flex-col gap-3'>
-            <input type="text" placeholder="Username" className="input input-bordered text-2xl w-96" />
-            <input type="password" placeholder="Password" className="input input-bordered text-2xl w-96" />
-            <button className='btn btn-warning'>Enter h3x|World</button>
+          <div className='flex flex-col gap-3 items-center justify-center'>
+            {
+              addedToWaitlist ?
+
+              <h1  className='text-xl mx-auto pb-3'>Joined the h<span className='text-[#d69e2e]'>3</span>x<span className='text-[#888888]'>|</span>World beta waitlist. Thank you and see you soon!</h1>
+              :
+              <h1  className='text-xl mx-auto pb-3'>Join the h<span className='text-[#d69e2e]'>3</span>x<span className='text-[#888888]'>|</span>World beta waitlist.</h1>
+            }
+            <input type="text" placeholder="Email" className="input input-bordered text-lg text-black w-80" disabled={addedToWaitlist} value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <button className='btn btn-warning w-80' onClick={() => addUserToWaitlist()} disabled={addedToWaitlist}>Join h3x|World</button>
           </div>
         </div>
       </div>
