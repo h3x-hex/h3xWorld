64827255422c0aae94e65e74be367a2eb2d5ff70
h3x-hex <160248326+h3x-hex@users.noreply.github.com>
2025-01-28 15:40:00 +0530

    Update Home.jsx

--- client/src/Routes/Home.jsx
+++ client/src/Routes/Home.jsx
@@ -17,6 +17,7 @@ function Home() {
   const isMobile = useMediaQuery({ query: '(max-width: 520px)' })
   const navigate = useNavigate();
   const featuresRef = useRef(null);
+  const homeRef = useRef(null);
   const [email, setEmail] = useState('');
   const [addedToWaitlist, setAddedToWaitlist] = useState(false);
 
@@ -45,12 +46,12 @@ function Home() {
     {
       isMobile ?
 
-      <div className='h-screen w-screen bg-stone-950 text-white'>
-        <div className='h-screen relative z-0'>
+      <div className='h-full w-screen bg-stone-950 text-white' ref={homeRef}>
+        <div className='h-full relative z-0'>
         <div className='flex flex-col pt-16 gap-16 '>
           <div className='flex flex-col mx-auto gap-3 pl-3'>
             <h1 className='text-5xl'>Welcome to h<span className='text-[#d69e2e]'>3</span>x<span className='text-[#888888]'>|</span>World</h1>
-            <h1 className='text-2xl'>Web3 Social Media</h1>
+            <h1 className='text-2xl'>Web3 Social Media for creators, artists, and entrepreneurs.</h1>
           </div>
           <div className='flex flex-col mx-auto pr-3 gap-3'>
             <div className='flex flex-col gap-3 items-center justify-center'>
@@ -63,9 +64,93 @@ function Home() {
               }
               <input type="text" placeholder="Email" className="input input-bordered text-lg text-white w-80" disabled={addedToWaitlist} value={email} onChange={(e) => setEmail(e.target.value)}/>
               <button className='btn btn-warning w-80' onClick={() => addUserToWaitlist()} disabled={addedToWaitlist}>Join h3x|World</button>
+              <div className='pt-10'><ArrowDown className='text-warning hover:cursor-pointer mx-auto' onClick={() => scrollToSection(featuresRef)}/></div>
             </div>
           </div>
         </div>
+        <div className="relative mt-28 border-b border-neutral-800 pt-6" ref={featuresRef}>
+            <div className="text-center">
+              <span className="bg-neutral-900 text-warning rounded-full h-12 text-3xl font-medium px-6 py-3">
+                Features
+              </span>
+              <h2 className="text-3xl sm:text-5xl lg:text-6xl pt-8 tracking-wide">
+                Easily build your 
+                <span className='font-bold text-warning'> {text}</span>
+                <span className='text-warning'><Cursor cursorStyle='|'/></span>
+              </h2>
+            </div>
+            <div className="flex flex-wrap mt-10 lg:mt-20">
+              {features.map((feature, index) => (
+                <div key={index} className="sm:w-1/2 lg:w-1/3">
+                  <div className="flex">
+                    <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-warning justify-center items-center rounded-full">
+                      {feature.icon}
+                    </div>
+                    <div className='w-8/12'>
+                      <h5 className="mt-1 mb-1 text-xl border-b-2 border-warning">{feature.text}</h5>
+                      <p className="text-md p-2 mb-20 text-white">
+                        {feature.description}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="text-center ">
+              <span className="bg-neutral-900 text-warning rounded-full h-12 text-3xl font-medium px-6 py-3">
+                h3xPremium
+              </span>
+              <p>for more storage (Free Tier 1GB)</p>
+              <div className="flex flex-col pt-8">
+                {pricingOptions.map((option, index) => (
+                  <div key={index} className=" p-2">
+                    <div className="p-10 border border-warning rounded-xl">
+                      <p className="text-4xl mb-8 border-b-2 border-warning">
+                        {option.title}
+                        {option.title === "Pro" && (
+                          <span className="text-warning bg-clip-text text-xl mb-4 ml-2">
+                            (Most Popular)
+                          </span>
+                        )}
+                      </p>
+                      <p className="mb-8">
+                        <span className="text-5xl mt-6 mr-2">{option.price}</span>
+                        <span className="text-neutral-400 tracking-tight">/Month</span>
+                      </p>
+                      <ul>
+                        {option.features.map((feature, index) => (
+                          <li key={index} className="mt-8 flex items-center">
+                            <CheckCircle2 />
+                            <span className="ml-2">{feature}</span>
+                          </li>
+                        ))}
+                      </ul>
+                      <a
+                        href="#"
+                        className="inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-warning hover:text-black border border-warning rounded-full transition duration-200"
+                      >
+                        Subscribe
+                      </a>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="text-center py-20 mx-auto">
+              <span className="bg-neutral-900 text-warning rounded-full h-12 text-3xl font-medium px-6 py-3">
+                Buy h3xCard
+              </span>
+              <p>Fun fact: Card is in 3D Space, you can move it around.</p>
+              <div className="flex flex-wrap py-8 h-96">
+                <Canvas className='w-screen h-screen bg-stone-900'>
+                  <ambientLight intensity={100}/>
+                  <OrbitControls enableZoom={false}/>
+                  <H3xCard scale={[0.4, 0.4, 0.4]}/>
+                </Canvas>
+              </div>
+              <button className='btn btn-warning rounded-full' onClick={() => scrollToSection(homeRef)}>Coming Soon...</button>
+            </div>
+          </div>
           <StarsCanvas />
       </div>
     
@@ -75,7 +160,7 @@ function Home() {
     
     </div>
     :
-    <div className='h-full bg-black text-white'>
+    <div className='h-full bg-black text-white' ref={homeRef}>
       <div className='h-full bg-black relative z-0'>
           <div className='flex flex-col pt-36 gap-36'>
             <div className='flex flex-col mx-auto text-center'>
@@ -170,6 +255,7 @@ function Home() {
               <span className="bg-neutral-900 text-warning rounded-full h-12 text-3xl font-medium px-6 py-3">
                 Buy h3xCard
               </span>
+              <p>Fun fact: Card is in 3D Space, you can move it around.</p>
               <div className="flex flex-wrap py-8 h-screen">
                 <Canvas className='w-96 h-96 bg-stone-900'>
                   <ambientLight intensity={100}/>
